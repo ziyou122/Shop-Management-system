49614dcf443cb6c3faac02d27b3131873d748ae1
ziyou <ziyou_geng@163.com>
2021-03-02 22:45:28 +0800

    2-6 develop Hello World interface http://127.0.0.1:8880/hello

--- src/main/java/com/wiki/wiki/config/WikiApplication.java
+++ src/main/java/com/wiki/wiki/config/WikiApplication.java
@@ -1,11 +1,13 @@
-package com.wiki.wiki;
+package com.wiki.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("com.wiki")
 @SpringBootApplication
 public class WikiApplication {
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
