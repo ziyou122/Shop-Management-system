b25d5b1d7bcc54571c02bd60a193ad3f075b2201
ziyou <ziyou_geng@163.com>
2021-03-03 14:28:25 +0800

    2-8 增加自定义配置项示例

--- src/main/java/com/wiki/wiki/controller/TestController.java
+++ src/main/java/com/wiki/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.wiki.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
     @RequestMapping("/hello")
     public String hello(){
-        return "hello world";
+        return "hello world" + testHello;
     }
 
     @PostMapping("/hello/post")
