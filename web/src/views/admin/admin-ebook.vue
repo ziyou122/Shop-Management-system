bbf06be549a2913c4613b908c54d7c8fde70bec7
ziyou <ziyou_geng@163.com>
2021-04-05 01:03:07 +0800

    6-12 编辑时复制对象；修改表单时，不会影响列表数据

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -4,9 +4,23 @@
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
       <p>
-        <a-button type="primary" @click="add()" size="large">
-          新增
-        </a-button>
+        <a-form layout="inline" :model="param">
+          <a-form-item>
+            <a-input v-model:value="param.name" placeholder="请输入关键词">
+              <template #prefix><LockOutlined style="color: rgba(0, 0, 0, 0.25)" /></template>
+            </a-input>
+          </a-form-item>
+          <a-form-item>
+            <a-button type="primary" @click="handleQuery({page: 1, size: pagination.pageSize})">
+              查询
+            </a-button>
+          </a-form-item>
+          <a-form-item>
+            <a-button type="primary" @click="add()">
+              新增
+            </a-button>
+          </a-form-item>
+        </a-form>
       </p>
       <a-table
           :columns="columns"
@@ -70,10 +84,13 @@
 import { defineComponent, onMounted, ref } from 'vue';
 import axios from 'axios';
 import { message } from 'ant-design-vue';
+import {Tool} from "@/util/tool";
 
 export default defineComponent({
   name: 'AdminEbook',
   setup() {
+    const param = ref();
+    param.value = {};
     const ebooks = ref();
     const pagination = ref({
       current: 1,
@@ -125,7 +142,13 @@ export default defineComponent({
      **/
     const handleQuery = (params: any) => {
       loading.value = true;
-      axios.get("/ebook/list", {params: {page: params.page, size: params.size}}).then((response) => {
+      axios.get("/ebook/list", {
+        params: {
+          page: params.page,
+          size: params.size,
+          name: param.value.name,
+        }
+      }).then((response) => {
         loading.value = false;
         const data = response.data;
         if (data.success) {
@@ -175,7 +198,7 @@ export default defineComponent({
     // 编辑
     const edit = (record: any) => {
       modalVisible.value = true;
-      ebook.value = record;
+      ebook.value = Tool.copy(record)
     }
 
     // 新增
@@ -211,12 +234,14 @@ export default defineComponent({
       columns,
       loading,
       handleTableChange,
+      handleQuery,
 
       edit,
       add,
       handleDelete,
 
       ebook,
+      param,
       modalVisible,
       modalLoading,
       handleModalOk
