6ac906af9fbb85853690828e6e204534f3f3e923
ziyou <ziyou_geng@163.com>
2021-04-05 22:08:30 +0800

    8-5 增加页面参数传递ebook，完成新增文档功能

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -88,10 +88,12 @@ import { defineComponent, onMounted, ref } from 'vue';
 import axios from 'axios';
 import { message } from 'ant-design-vue';
 import {Tool} from "@/util/tool";
+import {useRoute} from "vue-router";
 
 export default defineComponent({
   name: 'AdminDoc',
   setup() {
+    const route = useRoute();
     const param = ref();
     param.value = {};
     const docs = ref();
@@ -149,7 +151,7 @@ export default defineComponent({
     const modalLoading = ref(false);
     const handleModalOk = () => {
       modalLoading.value = true;
-      console.log(doc.value)
+      // console.log(doc.value)
       axios.post("/doc/save", doc.value).then((response) => {
         modalLoading.value = false;
         const data = response.data;
@@ -207,7 +209,9 @@ export default defineComponent({
     // 新增
     const add = (record: any) => {
       modalVisible.value = true;
-      doc.value = {};
+      doc.value = {
+        ebookId: route.query.ebookId
+      };
 
       treeSelectData.value = Tool.copy(level1.value) || [];
 
