0771e601e64de6344a61b5eb00ce76ceb4f86296
ziyou <ziyou_geng@163.com>
2021-04-06 22:36:12 +0800

    9-9 前端显示登录昵称，遗留问题：刷新后页面数据会消失

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -22,7 +22,10 @@
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
-      <a class="login-menu" @click="showLoginModal">
+      <a class="login-menu" v-show="user.id">
+        <span>您好：{{user.name}}</span>
+      </a>
+      <a class="login-menu" @click="showLoginModal" v-show="!user.id">
         <span>登录</span>
       </a>
     </a-menu>
@@ -57,7 +60,11 @@ declare let KEY: any;
 export default defineComponent({
   name: 'the-header',
   setup() {
-    // 用来登录
+    // 登陆后显示在页面上
+    const user = ref();
+    user.value = {};
+
+    // 用来登录和后端交互
     const loginUser = ref({
       loginName: "test",
       password: "test"
@@ -79,7 +86,7 @@ export default defineComponent({
         if (data.success) {
           loginModalVisible.value = false;
           message.success("登录成功！");
-
+          user.value = data.content;
           //store.commit("setUser", data.content);
         } else {
           message.error(data.message);
@@ -93,6 +100,7 @@ export default defineComponent({
       showLoginModal,
       loginUser,
       login,
+      user
     }
 
   }
