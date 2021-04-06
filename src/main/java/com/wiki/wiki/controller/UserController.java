f462c103caf1cabec5d4eabed0324b75491e93a6
ziyou <ziyou_geng@163.com>
2021-04-06 20:37:14 +0800

    9-5 密码加密传输和加密存储

--- src/main/java/com/wiki/wiki/controller/UserController.java
+++ src/main/java/com/wiki/wiki/controller/UserController.java
@@ -3,9 +3,10 @@ package com.wiki.wiki.controller;
 import com.wiki.wiki.req.UserQueryReq;
 import com.wiki.wiki.req.UserSaveReq;
 import com.wiki.wiki.resp.CommonResp;
-import com.wiki.wiki.resp.UserQueryResp;
 import com.wiki.wiki.resp.PageResp;
+import com.wiki.wiki.resp.UserQueryResp;
 import com.wiki.wiki.service.UserService;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -29,6 +30,7 @@ public class UserController {
 
     @PostMapping("/save")
     public CommonResp save(@Valid @RequestBody UserSaveReq req){
+        req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp resp = new CommonResp<>();
         userService.save(req);
         return resp;
