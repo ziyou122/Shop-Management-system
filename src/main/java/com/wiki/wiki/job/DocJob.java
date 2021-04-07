dc236e17d7bce4c89287e520c367de8c39bcc53d
ziyou <ziyou_geng@163.com>
2021-04-07 17:07:33 +0800

    10-6 增加定时器 定时执行电子书信息更新的sql, 增加日志

--- src/main/java/com/wiki/wiki/job/DocJob.java
+++ src/main/java/com/wiki/wiki/job/DocJob.java
@@ -11,17 +11,20 @@ import javax.annotation.Resource;
 @Component
 public class DocJob {
 
-   private static final Logger LOG = LoggerFactory.getLogger(DocJob.class);
+    private static final Logger LOG = LoggerFactory.getLogger(DocJob.class);
 
-   @Resource
-   private DocService docService;
+    @Resource
+    private DocService docService;
 
-   /**
-    * 每30s 更新电子书信息
-    */
-   @Scheduled(cron = "5/30 * * * * ?")
-   public void cron() {
+    /**
+     * 每30s 更新电子书信息
+     */
+    @Scheduled(cron = "5/30 * * * * ?")
+    public void cron() {
+       LOG.info("开始更新电子书下的文档数据");
+       long start = System.currentTimeMillis();
        docService.updateEbookInfo();
-   }
+        LOG.info("结束更新电子书下的文档数据，耗时：{}毫秒", System.currentTimeMillis() - start);
+    }
 
 }
