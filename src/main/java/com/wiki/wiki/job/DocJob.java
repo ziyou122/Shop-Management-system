52a94b2e9538d77437875332fc5fdd59b4be95a7
ziyou <ziyou_geng@163.com>
2021-04-07 17:24:44 +0800

    10-7 增加日志流水号 方便查看一个线程下的日志

--- src/main/java/com/wiki/wiki/job/DocJob.java
+++ src/main/java/com/wiki/wiki/job/DocJob.java
@@ -1,8 +1,10 @@
 package com.wiki.wiki.job;
 
 import com.wiki.wiki.service.DocService;
+import com.wiki.wiki.util.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -16,12 +18,17 @@ public class DocJob {
     @Resource
     private DocService docService;
 
+    @Resource
+    private SnowFlake snowFlake;
+
     /**
      * 每30s 更新电子书信息
      */
     @Scheduled(cron = "5/30 * * * * ?")
     public void cron() {
-       LOG.info("开始更新电子书下的文档数据");
+        // 增加日志流水号
+        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
+        LOG.info("开始更新电子书下的文档数据");
        long start = System.currentTimeMillis();
        docService.updateEbookInfo();
         LOG.info("结束更新电子书下的文档数据，耗时：{}毫秒", System.currentTimeMillis() - start);
