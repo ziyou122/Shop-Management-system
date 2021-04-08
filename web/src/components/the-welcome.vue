94752944fa41c20a934fce45f1cb4a1531a072e4
ziyou <ziyou_geng@163.com>
2021-04-08 14:31:31 +0800

    11-6 前端增加数值统计组件，显示统计数值

--- web/src/components/the-welcome.vue
+++ web/src/components/the-welcome.vue
@@ -1,18 +1,154 @@
 <template>
-  <h1>欢迎！！！！11</h1>
+  <div>
+<!--    <div class="tip">-->
+<!--      <div><b>示例网站说明：</b></div>-->
+<!--      <div>1. 统计数据是真实的，一分钟左右延时。</div>-->
+<!--      <div>2. 有文档被点赞会收到实时通知哦！</div>-->
+<!--      <div>3. 左侧菜单是动态加载的，登录后在分类管理可配置</div>-->
+<!--      <div>4. 文档树可无限级扩展，支持文字、图片、<b>视频</b></div>-->
+<!--      <div>你也想有个WIKI知识库吗？，<b>配套视频课程</b>：-->
+<!--        <a href="https://coding.imooc.com/class/474.html" target="_blank">-->
+<!--          《SpringBoot知识体系+Vue3全家桶 前后端分离 实战WIKI知识库系统》-->
+<!--        </a>-->
+<!--      </div>-->
+<!--    </div>-->
+
+    <a-row>
+      <a-col :span="24">
+        <a-card>
+          <a-row>
+            <a-col :span="8">
+              <a-statistic title="总浏览量" :value="statistic.viewCount">
+                <template #suffix>
+                  <UserOutlined />
+                </template>
+              </a-statistic>
+            </a-col>
+            <a-col :span="8">
+              <a-statistic title="总销售量" :value="statistic.voteCount">
+                <template #suffix>
+                  <like-outlined />
+                </template>
+              </a-statistic>
+            </a-col>
+            <a-col :span="8">
+              <a-statistic title="购买率" :value="statistic.voteCount / statistic.viewCount * 100"
+                           :precision="2"
+                           suffix="%"
+                           :value-style="{ color: '#cf1322' }">
+                <template #suffix>
+                  <like-outlined />
+                </template>
+              </a-statistic>
+            </a-col>
+          </a-row>
+        </a-card>
+      </a-col>
+    </a-row>
+    <br>
+    <a-row :gutter="16">
+      <a-col :span="12">
+        <a-card>
+          <a-row>
+            <a-col :span="12">
+              <a-statistic title="今日浏览" :value="statistic.todayViewCount" style="margin-right: 50px">
+                <template #suffix>
+                  <UserOutlined />
+                </template>
+              </a-statistic>
+            </a-col>
+            <a-col :span="12">
+              <a-statistic title="今日销售" :value="statistic.todayVoteCount">
+                <template #suffix>
+                  <like-outlined />
+                </template>
+              </a-statistic>
+            </a-col>
+          </a-row>
+        </a-card>
+      </a-col>
+      <a-col :span="12">
+        <a-card>
+          <a-row>
+            <a-col :span="12">
+              <a-statistic
+                  title="预计今日浏览量"
+                  :value="statistic.todayViewIncrease"
+                  :value-style="{ color: '#0000ff' }"
+              >
+                <template #suffix>
+                  <UserOutlined />
+                </template>
+              </a-statistic>
+            </a-col>
+            <a-col :span="12">
+              <a-statistic
+                  title="预计今日浏览增长"
+                  :value="statistic.todayViewIncreaseRateAbs"
+                  :precision="2"
+                  suffix="%"
+                  class="demo-class"
+                  :value-style="statistic.todayViewIncreaseRate < 0 ? { color: '#3f8600' } : { color: '#cf1322' }"
+              >
+                <template #prefix>
+                  <arrow-down-outlined v-if="statistic.todayViewIncreaseRate < 0"/>
+                  <arrow-up-outlined v-if="statistic.todayViewIncreaseRate >= 0"/>
+                </template>
+              </a-statistic>
+            </a-col>
+          </a-row>
+        </a-card>
+      </a-col>
+    </a-row>
+    <br>
+    <a-row>
+      <a-col :span="24" id="main-col">
+        <div id="main" style="width: 100%;height:300px;"></div>
+      </a-col>
+    </a-row>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent, onMounted, ref} from 'vue';
+import axios from 'axios';
 
-declare let hexMd5: any;
-declare let KEY: any;
+declare let echarts: any;
 
 export default defineComponent({
   name: 'the-welcome',
   setup() {
+    const statistic = ref();
+    statistic.value = {};
+    const getStatistic = () => {
+      axios.get('/ebook-snapshot/get-statistic').then((response) => {
+        const data = response.data;
+        if (data.success) {
+          const statisticResp = data.content;
+          statistic.value.viewCount = statisticResp[1].viewCount;
+          statistic.value.voteCount = statisticResp[1].voteCount;
+          statistic.value.todayViewCount = statisticResp[1].viewIncrease;
+          statistic.value.todayVoteCount = statisticResp[1].voteIncrease;
 
+          // 按分钟计算当前时间点，占一天的百分比
+          const now = new Date();
+          const nowRate = (now.getHours() * 60 + now.getMinutes()) / (60 * 24);
+          // console.log(nowRate)
+          statistic.value.todayViewIncrease = parseInt(String(statisticResp[1].viewIncrease / nowRate));
+          // todayViewIncreaseRate：今日预计增长率
+          statistic.value.todayViewIncreaseRate = (statistic.value.todayViewIncrease - statisticResp[0].viewIncrease) / statisticResp[0].viewIncrease * 100;
+          statistic.value.todayViewIncreaseRateAbs = Math.abs(statistic.value.todayViewIncreaseRate);
+        }
+      });
+    };
+
+    onMounted(() => {
+      getStatistic();
+      // // testEcharts();
+      // get30DayStatistic();
+    });
     return {
+      statistic
     }
 
   }
